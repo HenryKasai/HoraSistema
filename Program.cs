ff9e23a4bdedade0b0d515339cba79175b2f8c58
HenryKasai <henrykasai1775@gmail.com>
2020-05-22 22:20:32 -0300

    Exercício Resolvido.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime DataDoSistema = DateTime.Now;
+            int dia = DateTime.Now.Day;
+            int mês = DateTime.Now.Month;
+            int ano = DateTime.Now.Year;
+            int hora = DateTime.Now.Hour;
+            int minuto = DateTime.Now.Minute;
+            int segundo = DateTime.Now.Second;
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Neste momento é exatamente {DataDoSistema}");
+            Console.WriteLine("ou");
+            Console.WriteLine($"Dia {dia}, mês {mês}, ano {ano}, á exatamento {hora} horas, {minuto} minutos e {segundo} segundos.");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
